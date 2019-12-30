2bc678d30521fbffe23c1fb641371780c8c1349e
aditzhaki <57772714+aditzhaki@users.noreply.github.com>
2019-12-31 00:06:41 +0200

    עדכון 3 יום שני
    לאחר הוספת הערות

--- exercise2/Simulator.java
+++ exercise2/Simulator.java
@@ -21,7 +21,7 @@ public class Simulator<L,WO> {
 	 * @requires labelPipe != null && labelPipe has
 	 *           not been used in a previous addPipe()  or
 	 *           addFilter() call on this Simulatable<L> object
-	 * @effects adds to this.simulateGraph a new pipe Object  which has interface Simulatable<L> and named by
+	 * @effects adds to this Simulator a new pipe Object  which has interface Simulatable<L> and named by
 	 *  		the Label labelPipe.
 	 */
 	public void addPipe(L labelPipe, Simulatable<L> pipe)  {
@@ -33,7 +33,7 @@ public class Simulator<L,WO> {
 	 * @requires labelFilter != null && labelFilter has
 	 *           not been used in a previous addPipe()  or
 	 *           addFilter() call on this Simulatable<L> object
-	 * @effects adds to this.simulateGraph a new filter Object  which has interface Simulatable<L> and named by
+	 * @effects adds to this simulator a new filter Object  which has interface Simulatable<L> and named by
 	 *  		the Label labelFilter.
 	 */
 	public void addFilter(L labelFilter, Simulatable<L> filter) {
@@ -63,14 +63,13 @@ public class Simulator<L,WO> {
 		checkRep();
 	}
 	/**
-	 * @requires createSimulator(simName) && ((addPipe(parentName) &&
-	 *           addFilter(childName)) || (addFilter(parentName) &&
-	 *           addPipe(childName))) && edgeLabel != null && node named
-	 *           parentName has no other outgoing edge labeled edgeLabel 
-	 *           && node named childName has no other incoming edge labeled edgeLabel
-	 * @modifies simulator named simName
-	 * @effects Adds an edge from the node named parentName to the node named
-	 *          childName in the simulator named simName. The new edge's label
+     * @requires edgeLabel != null && srcLabel != null && destLabel != null
+     * 			 && Objects with srcLabel and destLabel already exist in graph.  
+     * 			 && node named srcLabel has no other outgoing edge labeled edgeLabel 
+	 *           && node named destLabel has no other incoming edge labeled edgeLabel
+	 * @modifies this.simulateGraph
+	 * @effects Adds an edge from the Node Object named srcLabel to the Node Object named
+	 *          destLabel in this Simulator. The new edge's label
 	 *          is the String edgeLabel.
 	 */
 	public void conecTwoObjects(L edgeLabel, L srcLabel, L destLabel) {
@@ -79,10 +78,16 @@ public class Simulator<L,WO> {
 		this.simulateGraph.addEdge(edgeLabel, srcLabel, destLabel);
 		checkRep();
 	}
+    /**
+     * @return this.simulateGraph
+     */
 	public BipartiteGraph<L> getBipartiteGraph(){
 		return simulateGraph;
 	}
-
+    /**
+     * @requires objLabel != null and Node with objLabel already exist at this graph
+     * @return Object of the Node with objLabel in this Simulator.
+     */
 	public Simulatable<L> getObj(L objLabel){
 		checkRep();
 		return ((Simulatable<L>) (this.simulateGraph.getObject(objLabel)));
