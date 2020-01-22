bcdc6269b8fa37aceccd6c5103a33c98a9aa21b5
e-fried <57075913+e-fried@users.noreply.github.com>
2020-01-22 20:12:59 +0200

    Update StepOrder.java

--- exercise4/StepOrder.java
+++ exercise4/StepOrder.java
@@ -5,13 +5,22 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Observable;
-
 import javax.swing.*;
+
+/**
+* This is an inherited class implementing PaintOrder,
+* this class represents a continous coloring order in which the top left panel is painted than the adjacent panel to the right
+* until the completzion of the first row, than on to the second row and so forth.
+*/
 
 public class StepOrder implements PaintOrder {
 
 	private Timer t;
-
+	
+	// Abs. Function:
+	// Represents the order in which the panels in the screen change color at every time unit. 
+	// Rep. Invariant:
+	// 
 	@Override
 	public void update(Observable arg0, Object panels) {
 		t = new javax.swing.Timer(40, new ActionListener() {
@@ -29,6 +38,10 @@ public class StepOrder implements PaintOrder {
 		});
 		t.start();
 	}
+	/**
+	 * @effects stops update of the panel colors.
+	 *          
+	 */
   public void stopRun() {
 		if (t != null)
 			t.stop();
