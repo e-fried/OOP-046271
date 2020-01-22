bba9f0df374d7031dbaab0c54b4f7b211d2e6689
e-fried <57075913+e-fried@users.noreply.github.com>
2020-01-22 19:44:52 +0200

    Update ColumnOrder.java

--- exercise4/ColumnOrder.java
+++ exercise4/ColumnOrder.java
@@ -11,9 +11,18 @@ import javax.swing.*;
 * this class represents a coloring order by columns, in which first column of panles is first painted than second ans so forth. interface for strategy design pattern of different orders by which to paint the panels
 */
 public class ColumnOrder implements PaintOrder {
+	// Abs. Function:
+	// Represents the order in which the panels in the screen change color at every time unit. 
+	// Rep. Invariant:
+	// 
 
 	private Timer t;
-
+	/**
+	 * @effects updates color of the various panels by calling paintPanel.
+	 * the order in which these panels are updated is the order held in the array order.
+	 * @requires panels !=null
+	 *          
+	 */
 	@Override
 	public void update(Observable arg0, Object panels ) {
 		int[] order = { 0, 5, 10, 15, 20, 1, 6, 11, 16, 21, 2, 7, 12, 17, 22, 3, 8, 13, 18, 23, 4, 9, 14, 19, 24 };
@@ -30,6 +39,10 @@ public class ColumnOrder implements PaintOrder {
 		});
 		t.start();
 	}
+	/**
+	 * @effects stops update of the panel colors.
+	 *          
+	 */
   public void stopRun() {
 		if (t != null)
 			t.stop();
